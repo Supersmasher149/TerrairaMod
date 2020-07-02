1e23838d6e770d747a3bc7915d81d5b809596a5d
Supersmasher149 <walter.gibbons@gmail.com>
2020-07-02 19:32:24 -0400

    Add project files.

---
+++ Items/basicItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+
+namespace NewSword.Items
+{
+    public class basicItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Basic Item");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.rare = 2;
+            item.value = 10;
+            item.maxStack = 999;
+        }
+    }
+}
---
+++ NewSword.cs
@@ -0,0 +1,13 @@
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Newtonsoft.Json.Bson;
+using System.ComponentModel;
+
+namespace NewSword
+{
+	public class NewSword : Mod
+	{
+	}
+
+}
