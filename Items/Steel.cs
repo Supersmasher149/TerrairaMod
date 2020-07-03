ee957246c8f1d1a559e1d4a1f1f09e141d94903b
Penguin <walter.gibbons@gmail.com>
2020-07-03 16:23:19 -0400

    things i dont rember

---
+++ Items/Steel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace NewSword.Items
+{
+    public class Steel : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Steel Bar");
+            Tooltip.SetDefault("steardy");
+        }
+        public override void SetDefaults()
+        {
+        }
+    }
+}
